913fe8317b8877569d82743c039999bbf7544858
leek-emperor <1332590314@qq.com>
2023-02-19 17:12:21 +0800

    test: 增加input和Breadcrumb的测试

--- src/Input/test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Input } from 'antd';
-
-const fath = () => {
-  const onChange = (val: any) => {
-    console.log(val);
-  };
-  return <Input onChange={onChange} />;
-};
